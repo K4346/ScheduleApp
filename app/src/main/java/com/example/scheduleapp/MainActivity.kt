bb734219c51cb5bbc6023804e67a028d288c75d5
K4346 <laeyenix@gmail.com>
2021-10-27 10:52:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.scheduleapp
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.scheduleapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_content_main
+        ) as NavHostFragment
+        binding.l.bnv.setupWithNavController(navHostFragment.navController)
+}
+}
---
+++ app/src/main/java/com/example/scheduleapp/ThirdFragment.kt
@@ -0,0 +1,26 @@
+package com.example.scheduleapp
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.scheduleapp.databinding.FragmentThirdBinding
+
+class ThirdFragment: Fragment() {
+    private var _binding: FragmentThirdBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+}
